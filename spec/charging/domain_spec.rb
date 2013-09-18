3f4984f4c0ae9463894b5c51b65c1d12a86ec66a
Celestino Gomes <celestino.gomes@myfreecomm.com.br>
2013-09-18 11:28:14 -0300

    more tests for finding domains by uuid via account

--- spec/charging/domain_spec.rb
+++ spec/charging/domain_spec.rb
@@ -93,30 +93,39 @@ describe Charging::Domain, :vcr do
       end
     end
 
+    it 'should raise if not response to success (200)' do
+      response_mock = double('AcceptedResponse', code: 202, to_s: 'AcceptedResponse')
+
+      described_class
+        .should_receive(:get_account_domain)
+        .with(account, uuid)
+        .and_return(response_mock)
+
+      expect {
+        described_class.find_by_uuid(account, uuid)
+      }.to raise_error Charging::Http::LastResponseError, 'AcceptedResponse'
+    end
+
     context 'for a valid uuid' do
-      let(:result) do
+      subject do
         VCR.use_cassette('finding a domain via account') do
           described_class.find_by_uuid(account, uuid)
         end
       end
 
-      it 'should raise if not response to success (200)' do
-        response_mock = double('AcceptedResponse', code: 202, to_s: 'AcceptedResponse')
-
-        described_class
-          .should_receive(:get_account_domain)
-          .with(account, uuid)
-          .and_return(response_mock)
-
-        expect {
-          described_class.find_by_uuid(account, uuid)
-        }.to raise_error Charging::Http::LastResponseError, 'AcceptedResponse'
-      end
-
       it 'should instanciate a domain' do
-        expect(result).to be_an_instance_of(Charging::Domain)
+        expect(subject).to be_an_instance_of(Charging::Domain)
       end
-    end
 
+      it 'should be a persisted instance' do
+        expect(subject).to be_persisted
+      end
+
+      its(:uri) { should eq 'http://sandbox.charging.financeconnect.com.br/account/domains/154932d8-66b8-4e6b-82f5-ebb1d32fe85d/' }
+      its(:uuid) { should eq uuid }
+      its(:etag) { should eq 'e11877e49b4ac65b4b8d96c16012a20254312e74' }
+      its(:token) { should eq '74QaWW3uSWKPPJVsBgBR6w==' }
+      its(:account) { should eq account }
+    end
   end
 end
