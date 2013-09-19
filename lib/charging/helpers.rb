5721fad1bbeedeab7b2caba3f0f1fa0f5457b435
Celestino Gomes <celestino.gomes@myfreecomm.com.br>
2013-09-19 18:45:56 -0300

    removed unnecessary helper methods

--- lib/charging/helpers.rb
+++ lib/charging/helpers.rb
@@ -1,26 +1,12 @@
 # encoding: utf-8
-require 'uri'
-
 module Charging
   module Helpers
     module_function
-
-    def urify(path, params = {})
-      query_params = params.any? ? "?#{URI.encode_www_form(params)}" : ''
-
-      "#{path}#{query_params}"
-    end
-
     def load_variables(object, attributes, hash)
       attributes.each do |attribute|
         value = hash.fetch(attribute, hash.fetch(attribute.to_s, nil))
         object.instance_variable_set "@#{attribute}", value
       end
-    end
-
-    def coalesce_int(value, default_value)
-      return default_value if value.to_i < 1
-      value
     end
 
     def required_arguments!(arguments)
@@ -31,10 +17,6 @@ module Charging
       end
 
       raise ArgumentError, errors.join(', ') if errors.any?
-    end
-
-    def filter_keys(hash, keys)
-      Hash[hash.find_all {|k,v| keys.include?(k) }]
     end
   end
 end
