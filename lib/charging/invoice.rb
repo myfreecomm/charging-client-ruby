54f2c29f7faa4ae562c3324f005f375731e2a4d1
Celestino Gomes <celestino.gomes@myfreecomm.com.br>
2013-09-27 13:07:43 -0300

    creating a new invoice at API

--- lib/charging/invoice.rb
+++ lib/charging/invoice.rb
@@ -2,21 +2,78 @@
 
 module Charging
   class Invoice < Base
-    ATTRIBUTES = [ 
+    ATTRIBUTES = [
       :kind, :amount, :document_number, :drawee, :due_date, 
       :charging_features, :supplier_name, :discount, :interest, :rebate,
       :ticket, :protest_code, :protest_days, :instructions, :demonstrative,
       :our_number
     ]
-    
-    READ_ONLY_ATTRIBUTES = [ :charge_account, :document_date, :paid ]
-    
+
+    READ_ONLY_ATTRIBUTES = [ :document_date, :paid ]
+
     attr_accessor(*ATTRIBUTES)
-    attr_reader(*READ_ONLY_ATTRIBUTES, :domain)
-    
-    def initialize(attributes, domain, response = nil)
+    attr_reader(*READ_ONLY_ATTRIBUTES, :domain, :charge_account)
+
+    def initialize(attributes, domain, charge_account, response = nil)
       super(attributes, response)
       @domain = domain
+      @charge_account = charge_account
+    end
+
+    # Creates current invoice at API.
+    #
+    # API method: <tt>POST /charge-accounts/:uuid/invoices/</tt>
+    #
+    # API documentation: https://charging.financeconnect.com.br/static/docs/charges.html#post-charge-accounts-uuid-invoices
+    def create!
+      @errors = []
+      raise 'can not create without a domain' if invalid_domain?
+      raise 'can not create wihtout a charge account' if invalid_charge_account?
+
+      @last_response = Invoice.post_charge_accounts_invoices(domain, charge_account, attributes)
+
+      raise Http::LastResponseError.new(last_response) if last_response.code != 201
+
+      reload_attributes_after_create!
+    rescue ::RestClient::Exception => exception
+      @last_response = exception.response
+
+      raise Http::LastResponseError.new(last_response)
+    ensure
+      @errors = [$ERROR_INFO.message] if $ERROR_INFO
+    end
+    
+    def self.load_persisted_invoice(attributes, response, domain, charge_account)
+      attributes.delete("charge_account")
+      validate_attributes!(attributes)
+      
+      Invoice.new(attributes, domain, charge_account, response)
+    end
+
+    private
+
+    def reload_attributes_after_create!
+      response = Http.get(last_response.headers[:location], domain.token)
+
+      new_invoice = Invoice.load_persisted_invoice(MultiJson.decode(response.body), response, domain, charge_account)
+
+      (COMMON_ATTRIBUTES + READ_ONLY_ATTRIBUTES).each do |attribute|
+        instance_variable_set "@#{attribute}", new_invoice.send(attribute)
+      end
+
+      self
+    end
+    
+    def self.post_charge_accounts_invoices(domain, charge_account, attributes)
+      Http.post("/charge-accounts/#{charge_account.uuid}/invoices/", domain.token, MultiJson.encode(attributes))
+    end
+
+    def invalid_domain?
+      domain.nil?
+    end
+
+    def invalid_charge_account?
+      charge_account.nil?
     end
   end
 end
--- spec/charging/invoice_spec.rb
+++ spec/charging/invoice_spec.rb
@@ -2,9 +2,24 @@
 
 require 'spec_helper'
 
-describe Charging::Invoice do
+describe Charging::Invoice, :vcr do
+  let(:charge_account) { double(:charge_account, uuid: '29e77bc5-0e70-444c-a922-3149e78d905b') }
   let(:domain) { double(:domain, token: 'QNTGvpnYRVC4HbHibDBUIQ==') }
-  let(:uuid) { '29e77bc5-0e70-444c-a922-3149e78d905b' }
+  let(:attributes) do
+    {
+      kind: 2,
+      amount: 123.45,
+      document_number: '000000000000001',
+      drawee: {
+        name: 'Nome do Sacado',
+        address: 'Rua do Carmo, 43',
+        city_state: 'Rio de Janeiro/RJ',
+        zipcode: '21345-999',
+        national_identifier: '37.818.380/0001-86'
+      },
+      due_date: '2020-12-31'
+    }
+  end
 
   context 'for new instance' do
     INVOICE_ATTRIBUTES = [ 
@@ -13,30 +28,31 @@ describe Charging::Invoice do
       :ticket, :protest_code, :protest_days, :instructions, :demonstrative,
       :our_number
     ]
-    
-    let(:response) { double(:response) }
-    
+
+    let(:response) { double(:response, headers: {}) }
+
     subject do
       attributes = Hash[*INVOICE_ATTRIBUTES.map {|attr| [attr, "#{attr} value"] }.flatten]
-      
-      described_class.new(attributes, domain, response)
+
+      described_class.new(attributes, domain, charge_account, response)
     end
-    
+
     INVOICE_ATTRIBUTES.each do |attribute|
       its(attribute) { should eq "#{attribute} value"}
     end
-    
-    [:uuid, :uri, :etag, :charge_account, :document_date, :paid].each do |attribute|
+
+    [:uuid, :uri, :etag, :document_date, :paid].each do |attribute|
       its(attribute) { should be_nil }
     end
-    
+
     its(:domain) { should eq domain }
+    its(:charge_account) { should eq charge_account }
     its(:last_response) { should eq response }
     its(:errors) { should eq [] }
-    
+
     specify('#persisted?') { expect(subject).to_not be_persisted }
     specify('#deleted?') { expect(subject).to_not be_deleted }
-    
+
     its(:attributes) do
       should eq({
         amount: 'amount value',
@@ -56,6 +72,48 @@ describe Charging::Invoice do
         demonstrative: 'demonstrative value', 
         our_number: 'our_number value'
       })
+    end
+  end
+
+  describe '#create!' do
+    it 'should require a domain and load errors' do
+      invoice = described_class.new(attributes, nil, charge_account)
+
+      expect(invoice.errors).to be_empty
+
+      expected_error = [StandardError, 'can not create without a domain']
+      expect { invoice.create! }.to raise_error(*expected_error)
+
+      expect(invoice.errors).to eq ['can not create without a domain']
+    end
+
+    it 'should require a charge account and load errors' do
+      invoice = described_class.new(attributes, domain, nil)
+
+      expect(invoice.errors).to be_empty
+
+      expected_error = [StandardError, 'can not create wihtout a charge account']
+      expect { invoice.create! }.to raise_error(*expected_error)
+
+      expect(invoice.errors).to eq ['can not create wihtout a charge account']
+    end
+
+    context 'when everything is OK' do
+      subject { described_class.new(attributes, domain, charge_account) }
+
+      before do
+        VCR.use_cassette('creating an invoice') do
+          subject.create!
+        end
+      end
+
+      [:uuid, :uri, :etag].each do |attribute|
+        its(attribute) { should_not be_nil }
+      end
+
+      it 'should be persisted' do
+        expect(subject).to be_persisted
+      end
     end
   end
 end
